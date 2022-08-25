5195febdaa77c2e0a55542ddf135a9306ab2e226
alfeushersandy <alfeusputra@gmail.com>
2022-08-25 16:44:51 +0700

    penambahan fitur register dan login

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -85,7 +85,8 @@
                 <span>Settings</span>
             </a>
 
-            <a href="#" class="sidebar-item" onclick="toggleActive(this)">
+            <a href="{{ route('logout') }}" class="sidebar-item" onclick="event.preventDefault();
+            document.getElementById('logout-form').submit();">
                 <!-- <img src="./assets/img/global/log-out.svg" alt=""> -->
 
                 <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -96,6 +97,9 @@
 
                 <span>Logout</span>
             </a>
+            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                @csrf
+            </form>
 
             
 
--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('dashboard.index');
+    return view('auth.login');
+});
+
+Route::get('/dashboard', function(){
+    return view('welcome');
 });
 
 //route kendaraan
